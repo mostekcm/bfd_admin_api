0a527d3646784874b16ab7cbb885814dc127320e
Carlos Mostek <carlos.mostek@auth0.com>
2017-03-08 01:55:50 -0600

    fix bad trace

--- src/service/OrderService.js
+++ src/service/OrderService.js
@@ -26,7 +26,7 @@ export default class OrderService {
     if (this.authenticated) return new Promise(resolve => resolve());
 
     const me = this;
-    console.debug("Carlos looking for service account creds: ", Object.keys(config('BFD_SERVICE_ACCOUNT_CREDS')).length);
+    logger.debug("Carlos looking for service account creds: ", Object.keys(config('BFD_SERVICE_ACCOUNT_CREDS')).length);
 
     return this.useServiceAccountAuth(config('BFD_SERVICE_ACCOUNT_CREDS'))
       .then(() => {
